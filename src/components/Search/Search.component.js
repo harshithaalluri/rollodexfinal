096fee94c2087c2a04da9dd34c635c64627df8c7
Harshitha Alluri <harshitha.alluri@qentelli.com>
2022-01-25 11:52:54 +0530

    sending all the files

---
+++ src/components/Search/Search.component.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Search = (props) =>{
+    return(
+        <input
+            name='search'
+            className='search-box'
+            type='password'
+            placeholder='search monsters'
+            onChange = {props.handleSearch}
+        />
+    )
+}
+
+
+export default Search
